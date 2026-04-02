b8ad48f1f4b970ade30c23262af29e4917f5ccb1
Santiago Ferraris <91099797+SantiagoFerraris@users.noreply.github.com>
2026-04-02 19:55:55 -0300

    fix: remove duplicate code in PublicLayout.tsx

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -22,24 +22,5 @@ export default function PublicLayout() {
       </Suspense>
     </>
   );
-}import { Outlet, useLocation } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import WhatsAppButton from './WhatsAppButton';
-import BackToTop from './BackToTop';
-
-export default function PublicLayout() {
-  const { pathname } = useLocation();
-
-  return (
-    <>
-      <Navbar />
-      <main key={pathname} className="animate-fade-in">
-        <Outlet />
-      </main>
-      <Footer />
-      <WhatsAppButton />
-      <BackToTop />
-    </>
-  );
 }
+
